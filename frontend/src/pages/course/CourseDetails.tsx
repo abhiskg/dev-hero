d1ee8b4d68401a0fef353a6399eb38ec8fc3b6f8
abhishek <abhiskg20@gmail.com>
2022-10-24 23:54:09 +0600

    All route created with dummy content

---
+++ frontend/src/pages/course/CourseDetails.tsx
@@ -0,0 +1,5 @@
+const CourseDetails = () => {
+  return <div>Details</div>;
+};
+
+export default CourseDetails;
--- frontend/src/router/rootRouter.tsx
+++ frontend/src/router/rootRouter.tsx
@@ -1,15 +1,42 @@
 import { createBrowserRouter } from "react-router-dom";
 import Root from "../layout/Root";
-import Home from "../pages/Home";
+import Login from "../pages/auth/Login";
+import Register from "../pages/auth/Register";
+import Blog from "../pages/blog/Blog";
+import CourseDetails from "../pages/course/CourseDetails";
+import Courses from "../pages/course/Courses";
+import ErrorPage from "../pages/error/ErrorPage";
+import Home from "../pages/main/Home";
 
 const rootRouter = createBrowserRouter([
   {
     path: "/",
     element: <Root />,
+    errorElement: <ErrorPage />,
     children: [
       {
         index: true,
         element: <Home />,
+      },
+      {
+        path: "/login",
+        element: <Login />,
+      },
+      {
+        path: "/register",
+        element: <Register />,
+      },
+      {
+        path: "/courses",
+        element: <Courses />,
+      },
+      {
+        path: "/course/:id",
+        element: <CourseDetails />,
+      },
+      {
+        path: "/blog",
+        element: <Blog />,
       },
     ],
   },
