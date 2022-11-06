c29e0e615624410486b2e4bb8f6a62f378d8e0d1
abhishek <abhiskg20@gmail.com>
2022-11-07 00:06:44 +0600

    Course card updated

--- frontend/src/components/cards/CourseCard.tsx
+++ frontend/src/components/cards/CourseCard.tsx
@@ -3,21 +3,21 @@ import { CourseType } from "../../types/courseType";
 
 const CourseCard = ({ course }: { course: CourseType }) => {
   return (
-    <div className="max-w-lg rounded-md bg-white p-4 shadow-md dark:bg-gray-900 dark:text-gray-100">
+    <div className=" rounded-md bg-white p-4 shadow-md dark:bg-gray-900 dark:text-gray-100">
       <div className="space-y-1">
         <img
           src={course.image}
           alt={course.alt}
-          className="block h-60 w-full rounded-md object-cover object-center dark:bg-gray-500"
+          className="block h-52 w-full rounded-md  dark:bg-gray-500"
         />
         <Link
           to={`/course/${course.id}`}
-          className="block text-lg font-semibold leading-snug dark:text-violet-400"
+          className="block text-lg font-semibold leading-snug hover:underline dark:text-violet-400"
         >
           {course.name}
         </Link>
         <p className="text-sm leading-snug dark:text-gray-400">
-          {course.author}
+          Author: {course.author}
         </p>
         <p className="font-medium">${course.price}</p>
       </div>
