ad58cacdf4c12e7e80078560aaf808ed33302827
abhishek <abhiskg20@gmail.com>
2022-11-06 23:14:25 +0600

    Faq updated with relevant content

--- frontend/src/pages/faq/Faq.tsx
+++ frontend/src/pages/faq/Faq.tsx
@@ -4,75 +4,53 @@ const Faq = () => {
   useDocTitle("FAQ");
 
   return (
-    <section className="pb-28 dark:bg-gray-800 dark:text-gray-100">
-      <div className="inner-width mx-auto flex flex-col justify-center p-4 md:p-8">
-        <h2 className="mb-12 text-center text-4xl font-bold leading-none sm:text-5xl">
-          Frequently Asked Questions
-        </h2>
-        <div className="flex flex-col divide-y divide-gray-700 sm:px-8 lg:px-12 xl:px-32">
-          <details>
-            <summary className="cursor-pointer py-2 outline-none ">
-              Optio maiores eligendi molestiae totam dolores similique?
-            </summary>
-            <div className="px-4 pb-4">
-              <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde
-                neque in fugiat magni, quas animi enim veritatis deleniti ex.
-                Impedit.
-              </p>
-            </div>
-          </details>
-          <details>
-            <summary className="cursor-pointer py-2 outline-none ">
-              Modi dolorem veritatis culpa quos consequuntur beatae itaque
-              excepturi perspiciatis?
-            </summary>
-            <div className="px-4 pb-4">
-              <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Est
-                aspernatur quae, eos explicabo odit minima libero veniam
-                similique quibusdam doloribus facilis ipsa accusantium vel
-                maiores corrupti! Libero voluptate a doloribus?
-              </p>
-            </div>
-          </details>
-          <details>
-            <summary className="cursor-pointer py-2 outline-none ">
-              Magni reprehenderit possimus debitis?
-            </summary>
-            <div className="space-y-2 px-4 pb-4">
-              <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut
-                voluptates aspernatur dolores in consequatur doloremque
-                inventore reprehenderit, consequuntur perspiciatis architecto.
-              </p>
-              <p>
-                Sed consectetur quod tenetur! Voluptatibus culpa incidunt
-                veritatis velit quasi cupiditate unde eaque! Iure, voluptatibus
-                autem eaque unde possimus quae.
-              </p>
-            </div>
-          </details>
-          <details>
-            <summary className="cursor-pointer py-2 outline-none ">
-              Magni reprehenderit possimus debitis?
-            </summary>
-            <div className="space-y-2 px-4 pb-4">
-              <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut
-                voluptates aspernatur dolores in consequatur doloremque
-                inventore reprehenderit, consequuntur perspiciatis architecto.
-              </p>
-              <p>
-                Sed consectetur quod tenetur! Voluptatibus culpa incidunt
-                veritatis velit quasi cupiditate unde eaque! Iure, voluptatibus
-                autem eaque unde possimus quae.
-              </p>
-            </div>
-          </details>
+    <div className="custom-width mx-auto">
+      <section className="pb-16 dark:bg-gray-800 dark:text-gray-100 sm:pb-40">
+        <div className="inner-width mx-auto flex flex-col justify-center">
+          <h2 className="header-style">Frequently Asked Questions</h2>
+          <div className="flex flex-col divide-y divide-gray-700 sm:px-8 lg:px-12 xl:px-32">
+            <details>
+              <summary className="cursor-pointer py-2 outline-none ">
+                How many courses are there in Dev Hero?
+              </summary>
+              <div className="px-4 pb-4">
+                <p>
+                  There are about 6 courses available right now, more coming
+                  soon.
+                </p>
+              </div>
+            </details>
+            <details>
+              <summary className="cursor-pointer py-2 outline-none ">
+                When I can get discount in premium membership?
+              </summary>
+              <div className="px-4 pb-4">
+                <p>We mainly focus on quality so we offer no discounts.</p>
+              </div>
+            </details>
+            <details>
+              <summary className="cursor-pointer py-2 outline-none ">
+                Does the course will held online and offline?
+              </summary>
+              <div className="space-y-2 px-4 pb-4">
+                <p>
+                  Presently, we are offering online course, later on we will
+                  move to offline.
+                </p>
+              </div>
+            </details>
+            <details>
+              <summary className="cursor-pointer py-2 outline-none ">
+                How many members are there in dev hero team?
+              </summary>
+              <div className="space-y-2 px-4 pb-4">
+                <p>Currently, 20 members are working actively in Dev Hero.</p>
+              </div>
+            </details>
+          </div>
         </div>
-      </div>
-    </section>
+      </section>
+    </div>
   );
 };
 
